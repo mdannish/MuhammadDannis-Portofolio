c9c4071ecc55c8d7be86359c3b2e36c8f69c9d9c
mdannish <154731824+mdannish@users.noreply.github.com>
2025-12-25 23:54:34 +0700

    Update page.tsx

--- app/work/[slug]/page.tsx
+++ app/work/[slug]/page.tsx
@@ -312,7 +312,7 @@ export default function WorkGallery({
           <motion.div
             key={item.id}
             className="work-card"
-            style={{ ["--row" as string]: item.span }}
+            style={{ ["--row" as "--row"]: item.span }}
             layoutId={`card-${slug}-${item.id}`}
             onClick={() => setActive(item)}
             whileHover={{ scale: 0.98 }}
